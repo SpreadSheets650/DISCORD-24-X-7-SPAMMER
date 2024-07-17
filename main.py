64f54ab45626ffb3713afa15a339f0f9e3eeac22
Zenitsu14536 <piplup8523@gmail.com>
2024-07-18 05:29:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 channelID = PUT THE CHANNEL ID
 headers = {
     "authorization":
-    "YOUR TOKEN HERE"
+    "MTI1Mzk5MDU2OTI1NzA3ODc4NA.GGgkt9.LLIrgkIOq3FyZXqC8TKLUc9sVWnTW0zo2gsL20"
 }
 keep_alive()
 file = open("text.txt", "r")
@@ -13,6 +13,6 @@ lines = file.readlines()
 while True:
     for line in lines:
         requests.post(
-            f"https://discord.com/api/v9/channels/{channelID}/messages",
+            f"https://discord.com/api/v9/channels/{1254091637756526695}/messages",
             headers=headers,
             json={"content": line})
